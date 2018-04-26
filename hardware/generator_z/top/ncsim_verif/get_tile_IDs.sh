b6608cfc598a123fd7f5c365ff1f4540e2dd9d05
Alex Carsello <ajcars@stanford.edu>
2018-04-26 13:25:59 -0700

    Create all feature test

--- hardware/generator_z/top/ncsim_verif/get_tile_IDs.sh
+++ hardware/generator_z/top/ncsim_verif/get_tile_IDs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CGRA_INFO_PATH='.'
+CGRA_INFO_PATH='..'
 grep -Po "(?<=<tile type='io1bit' tile_addr='0x).*(?=' r)" $CGRA_INFO_PATH/cgra_info.txt > io1bit_IDs.txt
 grep -Po "(?<=<tile type='io16bit' tile_addr='0x).*(?=' r)" $CGRA_INFO_PATH/cgra_info.txt > io16bit_IDs.txt
 grep -Po "(?<=<tile type='pe_tile_new' tile_addr='0x).*(?=' r)" $CGRA_INFO_PATH/cgra_info.txt > pe_IDs.txt

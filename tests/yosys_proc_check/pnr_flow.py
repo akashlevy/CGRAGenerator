8f35cc7184b8f9a2ef03174ab73d237afd8eb85a
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-06-12 15:54:00 -0700

    Now producing usable bitstream from pnr_flow

--- tests/yosys_proc_check/pnr_flow.py
+++ tests/yosys_proc_check/pnr_flow.py
@@ -8,13 +8,26 @@ gen = '~/CGRAGenerator/'
 app_path = '{0}bitstream/bsbuilder/testdir/examples/'.format(gen)
 app_name = 'pointwise'
 app_path = '{0}{1}'.format(app_path, app_name)
+bsdir = '{0}/bitstream/bsbuilder/'.format(gen)
 
 serpent_path = '{0}bitstream/bsbuilder/serpent.py'.format(gen)
 
+# Generate .dot file from mapped coreir
 gen_dot_cmd = '{0}testdir/graphcompare/json2dot.py < {1}_mapped.json > {2}.dot '.format(gen, app_path, app_name)
 print 'Generate dot command =', gen_dot_cmd
 run_cmd(gen_dot_cmd)
 
+# Place and route the .dot file using serpent and produce .bsb file
 serpent_cmd = '{0} -v {1}.dot -o {1}.bsb'.format(serpent_path, app_name)
 print 'Serpent command =', gen_dot_cmd
 run_cmd(serpent_cmd)
+
+# Build bitstream .bsa file from .bsb
+build_bsa_cmd = '{0}bsbuilder.py {1}.bsb > {1}.bsa'.format(bsdir, app_name)
+print 'Build .bsa command = ', build_bsa_cmd
+run_cmd(build_bsa_cmd)
+
+# Remove extra lines from bitstream
+remove_lines_cmd = './remove_config_blanks.sh {0}.bsa > {0}_only_config_lines.bsa'.format(app_name)
+print 'Remove lines command', remove_lines_cmd
+run_cmd(remove_lines_cmd)

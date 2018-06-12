05ed9b953bc3e601a3e609cf2c72a4547c7405ec
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-06-12 15:44:06 -0700

    Now have serpent running on bsb file

--- tests/yosys_proc_check/pnr_flow.py
+++ tests/yosys_proc_check/pnr_flow.py
@@ -9,7 +9,12 @@ app_path = '{0}bitstream/bsbuilder/testdir/examples/'.format(gen)
 app_name = 'pointwise'
 app_path = '{0}{1}'.format(app_path, app_name)
 
-gen_dot_cmd = '{0}testdir/graphcompare/json2dot.py < {1}_mapped.json > {2}.dot '.format(gen, app_path, app_name)
+serpent_path = '{0}bitstream/bsbuilder/serpent.py'.format(gen)
 
+gen_dot_cmd = '{0}testdir/graphcompare/json2dot.py < {1}_mapped.json > {2}.dot '.format(gen, app_path, app_name)
 print 'Generate dot command =', gen_dot_cmd
 run_cmd(gen_dot_cmd)
+
+serpent_cmd = '{0} -v {1}.dot -o {1}.bsb'.format(serpent_path, app_name)
+print 'Serpent command =', gen_dot_cmd
+run_cmd(serpent_cmd)

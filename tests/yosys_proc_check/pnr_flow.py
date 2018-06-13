791d0b5e79e323f8048d1b9c4b1ec9da5b87738c
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-06-13 12:13:55 -0700

    Snake test is running!

--- tests/yosys_proc_check/pnr_flow.py
+++ tests/yosys_proc_check/pnr_flow.py
@@ -1,41 +1,53 @@
 import os
 
 def run_cmd(cmd):
+    print 'Running command', cmd
     res = os.system(cmd)
     assert(res == 0)
 
 
 gen = '~/CGRAGenerator/'
-app_path = '{0}bitstream/bsbuilder/testdir/examples/'.format(gen)
-app_name = 'pointwise'
+#app_path = '{0}bitstream/bsbuilder/testdir/examples/'.format(gen)
+#app_path = '~/CGRAMapper/examples/'.format(gen)
+app_path = './'
+app_name = 'add_five'
+#app_name = 'add_2_input'
 app_path = '{0}{1}'.format(app_path, app_name)
 bsdir = '{0}/bitstream/bsbuilder/'.format(gen)
 
 serpent_path = '{0}bitstream/bsbuilder/serpent.py'.format(gen)
+mapper_path = '~/CGRAMapper/'
+
+# Map the coreir file
+map_cmd = '{0}bin/mapper {1}.json {2}_mapped.json'.format(mapper_path, app_path, app_name)
+run_cmd(map_cmd)
+# print 'Running command', map_cmd
+# res = os.system(map_cmd)
+
+#run_cmd(map_cmd)
 
 # Generate .dot file from mapped coreir
-gen_dot_cmd = '{0}testdir/graphcompare/json2dot.py < {1}_mapped.json > {2}.dot '.format(gen, app_path, app_name)
-print 'Generate dot command =', gen_dot_cmd
+gen_dot_cmd = '{0}testdir/graphcompare/json2dot.py < {1}_mapped.json > {1}.dot '.format(gen, app_name)
 run_cmd(gen_dot_cmd)
 
 # Place and route the .dot file using serpent and produce .bsb file
 serpent_cmd = '{0} -v {1}.dot -o {1}.bsb'.format(serpent_path, app_name)
-print 'Serpent command =', gen_dot_cmd
 run_cmd(serpent_cmd)
 
 # Build bitstream .bsa file from .bsb
 build_bsa_cmd = '{0}bsbuilder.py {1}.bsb > {1}.bsa'.format(bsdir, app_name)
-print 'Build .bsa command = ', build_bsa_cmd
 run_cmd(build_bsa_cmd)
 
+# Reorder bitstream to avoid loops
+run_cmd('{0}verilator/generator_z_tb/bin/reorder.csh {1}.bsa > {1}_reordered.bsa'.format(gen, app_name))
+
 # Remove extra lines from bitstream
-remove_lines_cmd = './remove_config_blanks.sh {0}.bsa > {0}_only_config_lines.bsa'.format(app_name)
-print 'Remove lines command', remove_lines_cmd
+remove_lines_cmd = './remove_config_blanks.sh {0}_reordered.bsa > {0}_only_config_lines.bsa'.format(app_name)
 run_cmd(remove_lines_cmd)
 
-# TODO: Add FlatCircuit-like automatic testbench generation
+# Compile and run VCS on the new bitstream
+# TODO: Add automatic testbench generation
 compile_vcs_cmd = 'vcs -assert disable +nbaopt +rad +nospecify +notimingchecks -ld gcc-4.4 +vcs+lic+wait -licqueue +cli -sverilog -full64 +incdir+/hd/cad/synopsys/dc_shell/latest/packages/gtech/src_ver/ +incdir+/hd/cad/synopsys/dc_shell/latest/dw/sim_ver/ -y /hd/cad/synopsys/dc_shell/latest/dw/sim_ver/ -CFLAGS \'-O3 -march=native\' test_pointwise.v ../../hardware/generator_z/top/genesis_verif/*.v ../../hardware/generator_z/top/genesis_verif/*.sv -top test'
-print 'Compile vcs command =', compile_vcs_cmd
 run_cmd(compile_vcs_cmd)
 
 run_cmd('./simv')
--- tests/yosys_proc_check/run_custom_bs_test.py
+++ tests/yosys_proc_check/run_custom_bs_test.py
@@ -1,5 +1,7 @@
 import os
 
+gen = '~/CGRAGenerator/'
+
 def run_cmd(cmd):
     res = os.system(cmd)
     assert(res == 0)
@@ -9,7 +11,9 @@ run_cmd('python make_snake_bsb.py > add_2_input.bsb')
 build_bs_cmd = '~/CGRAGenerator/bitstream/bsbuilder/bsbuilder.py add_2_input.bsb > add_2_input_tmp.bsa'
 run_cmd(build_bs_cmd)
 
-run_cmd('./remove_config_blanks.sh add_2_input_tmp.bsa > add_2_input.bsa')
+app_name = 'add_2_input_tmp'
+run_cmd('{0}verilator/generator_z_tb/bin/reorder.csh {1}.bsa > {1}_reordered.bsa'.format(gen, app_name))
+run_cmd('./remove_config_blanks.sh add_2_input_tmp_reordered.bsa > add_2_input.bsa')
 
 run_cmd('./vcs_run_test.sh')
 run_cmd('./simv')

8d07067d96b3f7d1f616c5d91b3379139272c407
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-07-11 16:14:52 -0700

    Now have a configuration register in the inputs

--- hardware/generator_z/cb/build_cb_top.py
+++ hardware/generator_z/cb/build_cb_top.py
@@ -1,4 +1,6 @@
 import os
+import math
+import mantle as mantle
 
 import magma as m
 
@@ -6,10 +8,50 @@ import magma as m
 def define_connect_box(width, num_tracks, has_constant, default_value, feedthrough_outputs):
     class ConnectBox(m.Circuit):
         name = f"connect_box_width_width_{str(width)}_num_tracks_{str(num_tracks)}_has_constant{str(has_constant)}_default_value{str(default_value)}_feedthrough_outputs_{feedthrough_outputs}"
-        IO = []
+        CONFIG_DATA_WIDTH = 32
+        CONFIG_ADDR_WIDTH = 32
 
+        IO = ["clk", m.In(m.Clock),
+              "reset", m.In(m.Reset),
+              "config_en", m.In(m.Bit),
+              "config_data", m.In(m.Bits(CONFIG_DATA_WIDTH)),
+              "config_addr", m.In(m.Bits(CONFIG_ADDR_WIDTH)),
+              "read_data", m.Out(m.Bits(CONFIG_DATA_WIDTH)),
+              "out", m.Out(m.Bits(width))]
+
+        for i in range(0, num_tracks):
+            if (feedthrough_outputs[i] == '1'):
+                IO.append("in_" + str(i))
+                IO.append(m.In(m.Bits(width)))
+            
         @classmethod
         def definition(io):
+            feedthrough_count = num_tracks
+            for i in range(0, len(feedthrough_outputs)):
+                feedthrough_count -= feedthrough_outputs[i] == '1'
+
+            mux_sel_bit_count = int(math.ceil(math.log(num_tracks - feedthrough_count + has_constant, 2)))
+
+            constant_bit_count = has_constant * width
+
+            config_bit_count = mux_sel_bit_count + constant_bit_count
+
+            config_reg_width = int(math.ceil(config_bit_count / 32.0)*32)
+
+            config_addrs_needed = int(math.ceil(config_bit_count / 32.0))
+
+            init_value = []
+
+            CONFIG_DATA_WIDTH = 32
+            CONFIG_ADDR_WIDTH = 32
+            
+            for i in range(0, CONFIG_DATA_WIDTH):
+                init_value.append(1)
+                
+            config_cb = mantle.Register(CONFIG_DATA_WIDTH,
+                                        init=0,
+                                        has_ce=False,
+                                        has_reset=True)
             return
 
     return ConnectBox

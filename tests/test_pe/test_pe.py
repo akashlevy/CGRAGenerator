8873c143dd541083a672764ead9853d6d5436a9d
Lenny Truong <lenny@stanford.edu>
2018-12-14 13:52:57 -0800

    Add note about issue 69

--- tests/test_pe/test_pe.py
+++ tests/test_pe/test_pe.py
@@ -234,10 +234,10 @@ def test_input_modes(signed, worker_id, input_modes):
 
     run_verilator_test('test_pe_unq1', f'harness', 'test_pe_unq1', build_directory)
 
-def test_lut(strategy, signed, lut_code, worker_id): #, random_op):
-    # op = random_op
-    op = choice(ops) # fail?
-    # op = "add"     # pass
+def test_lut(strategy, signed, lut_code, worker_id, random_op):
+    # NOTE: In the past, we have seen issues with this test randomly failing,
+    # see https://github.com/StanfordAHA/CGRAGenerator/issues/69 for more info
+    op = choice(ops)
     flag_sel = 0xE  # Lut output
     bit2_mode = 0x2  # BYPASS
     bit1_mode = 0x2  # BYPASS
